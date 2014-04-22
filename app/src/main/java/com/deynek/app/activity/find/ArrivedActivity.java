ac7179fd5386041cea89e907e54028b6268d86f4
Selman K. <selmanhalid@hotmail.com>
2014-04-21 19:57:53 -0700

    pick location activity is created on click
    (find another)

--- app/src/main/java/com/deynek/app/activity/find/ArrivedActivity.java
+++ app/src/main/java/com/deynek/app/activity/find/ArrivedActivity.java
@@ -2,19 +2,14 @@ package com.deynek.app.activity.find;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.deynek.app.R;
 import com.deynek.app.activity.MainActivity;
-import com.deynek.app.activity.mark.LeavingSpotActivity;
 import com.deynek.app.model.MyActivity;
 import com.deynek.app.session.ApplicationStateManager;
 import com.deynek.app.session.ParkInfoManager;
-import com.google.android.gms.maps.model.LatLng;
 
 public class ArrivedActivity extends MyActivity {
 
@@ -42,7 +37,7 @@ public class ArrivedActivity extends MyActivity {
 
     public void onFindAnotherClick(View v) {
         ApplicationStateManager.saveState(ApplicationStateManager.STATES.DEFAULT);
-        Intent i = new Intent(getApplicationContext(), FindSpotActivity.class);
+        Intent i = new Intent(getApplicationContext(), PickLocationActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
         finish();
